5f2dbe0564edf71e432a4e2557ed03c1ad685347
Sean-Bertolini <90006585+Sean-Bertolini@users.noreply.github.com>
2022-03-21 10:31:29 -0400

    Add files via upload

--- highway-hero/script.js
+++ highway-hero/script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
-  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", makeResponsive: true, actionRadius: 100, timeBetweenNPCs: 7500, npcCollisionRadius: 1000, actionDuration:2000, jumpHeight: 200, numOfCollectables: 2, removeCollectableAfterCollected: true, npcMoveToX: 175, removeNPCAfterInteracted: false, typesOfNPC: 100, removeNPCAfterInteractedDelay: 5000});
+  mugen.initialize({titleScreenImageURL: "./img/title-screen.gif", preGameScreenImageURLs: ["./img/pregame-screen-1.png", "./img/pregame-screen-2.png"] , makeResponsive: true, actionRadius: 100, timeBetweenNPCs: 7500, npcCollisionRadius: 1000, removeNPCAfterInteractedDelay: 50000, actionDuration:1450, jumpHeight: 200, numOfCollectables: 2, removeCollectableAfterCollected: true, npcMoveToX: 190, removeNPCAfterInteracted: false, typesOfNPC: 100,});
 });
